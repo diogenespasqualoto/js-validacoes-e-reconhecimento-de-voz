25061b1193c7b8b9519eea70be839fbea2c4d921
diogenespasqualto <diogenespasqualoto147@gmail.com>
2023-02-15 17:45:42 -0300

    Validações 2.0

--- app/validacoes.js
+++ app/validacoes.js
@@ -1,3 +1,21 @@
-function verificaSeOChutePossuiUmValorValido(){
-    
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (Number.isNaN(numero)) {
+        console.log('valor inválido')
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        console.log(`Valor Inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}`)
+    }
+
+
+    function chuteForInvalido(numero) {
+        return Number.isNaN(numero)
+    }
+}
+
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
 }
